4303512c25e2fdac07854ff57d05138ab73bd009
Dylan <dylan.moulin9272@gmail.com>
2025-03-31 17:34:05 +0200

    Ajout du formulaire HTML (non fini)

--- TP1.php
+++ TP1.php
@@ -45,7 +45,70 @@ function searchTitanic(){
 
     };
 
-
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titanic</title>
+</head>
+<body>
+    <form action="TP1.php" method="get">
+        <ul>
+            <li>
+                <label for="gender">
+                    Le genre de la personne : 
+                    <input type="text" placeholder="Genre" id="gender">
+                </label>
+            </li>
+            <li>
+                <label for="age">
+                    L'âge de la personne : 
+                    <input type="text" placeholder="Âge" id="gender">
+                </label>
+            </li>
+            <li>
+                <label for="classe">
+                    La classe de transport de la personne : 
+                    <select name="classe" id="classe" >
+                        <option value="">Sélectionner la classe</option>
+                        <option value="premiereClasse">1</option>
+                        <option value="secondeClasse">2</option>
+                        <option value="troisièmeClasse">3</option>
+                    </select>
+                </label>
+            </li>
+            <li>
+                <label for="survived">
+                    La personne a survécu :
+                    <input type="radio" id="survived" name="survived">
+                </label><br>
+                <label for="notSurvived">
+                    La personne n'a pas survécu :
+                    <input type="radio" id="notSurvived" name="notSurvived">
+                </label>
+            </li>
+            <li>
+                <label for="embarkment">
+                    <select name="embarkment" id="embarkment">
+                        <option value="S">Southampton</option>
+                        <option value="Q">Quechua</option>
+                        <option value="C">Château</option>
+                    </select>
+                </label>
+            </li>
+            <li>
+                <label for="gender">
+                    <input type="text" placeholder="Genre" id="gender">
+                </label>
+            </li>
+            <li>
+                <label for="gender">
+                    <input type="text" placeholder="Genre" id="gender">
+                </label>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
