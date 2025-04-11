5b42814e9e680667dcb1a07e41ccebd02f56d677
Dylan <dylan.moulin9272@gmail.com>
2025-04-11 20:10:09 +0200

    MAJ du TP : compteur.php

--- TP_Grafikart/functions/compteur.php
+++ TP_Grafikart/functions/compteur.php
@@ -1,15 +1,23 @@
 <?php
 
 function ajouter_vue (){
+    $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "functions" . DIRECTORY_SEPARATOR . "compteur.txt";
+    $compteur = 1;
+    if (file_exists($file)) {
+        $compteur = (int)file_get_contents($file);
+        $compteur++;
+    } 
+    file_put_contents($file, $compteur);
+}
 
-    if (file_exists('compteur.txt')) {
-        $file = fopen('compteur.txt', 'r+');
-        $compteur = file_get_contents($file)
-        fwrite($file, $compteur + 1)
-    } else {
-        file_put_contents($file, '1');
-    }
-    fclose($file);
+function nombre_vues () : string {
+    $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "functions" . DIRECTORY_SEPARATOR . "compteur.txt";
+    return file_get_contents($file);
+}
+
+function reset_compteur () {
+    $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "functions" . DIRECTORY_SEPARATOR . "compteur.txt";
+    file_put_contents($file, '0');
 }
 
 ?>
--- TP_Grafikart/index.php
+++ TP_Grafikart/index.php
@@ -1,19 +1,23 @@
-<?php require dirname(__DIR__) . DIRECTORY_SEPARATOR . "TP_Grafikart" . DIRECTORY_SEPARATOR. 'functions' . DIRECTORY_SEPARATOR . 'compteur.php' ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TP Grafikart PHP</title>
-</head>
-<body>
-    <?php require dirname(__DIR__) . DIRECTORY_SEPARATOR . "elements" . DIRECTORY_SEPARATOR . "navbar.php"; ?>
-    <header>
-        <h1 class="main-title">Mon site</h1>
-    </header>
-    <footer>
-        <h2 class="compteur">Compteur : <?= $compteur ?> </h2> 
-    </footer>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TP Grafikart PHP</title>
+    </head>
+    <body>
+        <?php require dirname(__DIR__) . DIRECTORY_SEPARATOR . "TP_Grafikart" . DIRECTORY_SEPARATOR . "elements" . DIRECTORY_SEPARATOR . "navbar.php"; ?>
+        <header>
+            <h1 class="main-title">Mon site</h1>
+        </header>
+        <footer>
+            <?php 
+            require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "TP_Grafikart" . DIRECTORY_SEPARATOR. 'functions' . DIRECTORY_SEPARATOR . 'compteur.php';
+            ajouter_vue(); 
+            $nbVues = nombre_vues();
+            ?>
+            <p>Il y a eu <?= $nbVues; ?> visite<?php if ($nbVues > 1):?>s<?php endif; ?> sur cette page. </p> 
+        </footer>
+    </body>
 </html>
